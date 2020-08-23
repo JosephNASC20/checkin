edc17683dc07942f72c1787ce5458e2dcf74c3a9
JosephNASC20 <yolpho23lofty@gmail.com>
2020-08-23 17:04:36 -0400

    worked on attendance to best of my ability

---
+++ checkin/attendance/attendance.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("classlistbtn");
+let selection;
+button.addEventListener("click",viewClassList());
+
+function viewClassList(event){
+    var select = document.getElementById("classlist");
+   return (Math.ceil(Math.random() * 100))
+
+}
